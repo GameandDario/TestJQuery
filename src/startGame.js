f4ab00e22c12b9ac7380a356e094bb33b852008f
Stéphane <stephane.sokeo@gmail.com>
2022-01-30 11:52:24 +0100

    set Instruction Slider & function changeTurn()

--- src/startGame.js
+++ src/startGame.js
@@ -20,14 +20,30 @@ $(function () {
     $("td").each(function (index, element) {
       let random = Math.floor(Math.random() * 10);
       if (random < 8) {
-      //plus de la moitié de cellules sont accessibles
-      // element == this
-      $(element).addClass("w3-lime").attr("accessible", true);
+        //plus de la moitié de cellules sont accessibles
+        // element == this
+        $(element).addClass("w3-lime").attr("accessible", true);
       } else {
         $(element).addClass("w3-dark-gray").attr("accessible", false);
       }
     });
 
+    $("#flipCommandes").click(function () {
+      $("#panelCommandes").slideToggle("slow");
+    });
+
+    function isPlayer1Turn() {
+      let randNb = Math.floor(Math.random() * 4); // 0, 1, 2, 3
+      console.log('randNb', randNb);
+      if (randNb % 2 == 0) {
+        player1.turn = true;
+        player2.turn = false;
+      } else {
+        player1.turn = false;
+        player2.turn = true;
+      }
+    }
+    
     //PERSO 1 : parmi les celllules accessibles en choisir une au hasard et la spécifiée comme personnage 1
     let accessibles = $("[accessible = true]");
     let pers1 = accessibles[Math.floor(Math.random() * accessibles.length)];
@@ -42,12 +58,17 @@ $(function () {
 
     player1.X = parseInt($(pers1).attr("x"));
     player1.Y = parseInt($(pers1).attr("y"));
-    player1.name = $(pers1) .attr("name");
+    player1.name = $(pers1).attr("name");
     player1.moves = parseInt($(pers1).attr("moves"));
-    player1.turn = true;
+    //player1.turn = true;
+    player1.health = 10;
+    player1.weapon = { name: "lambda", strength: 10 };
 
     //PERSO2
     let pers2 = accessibles[Math.floor(Math.random() * accessibles.length)];
+
+    //Test position pers2
+    console.log("pers2", pers2);
     $(pers2)
       .removeClass("w3-lime")
       .addClass("w3-blue")
@@ -59,14 +80,20 @@ $(function () {
     let player2 = $(pers2);
     player2.X = parseInt($(pers2).attr("x"));
     player2.Y = parseInt($(pers2).attr("y"));
-    player2.name = $(pers2) .attr("name");
+    player2.name = $(pers2).attr("name");
     player2.moves = parseInt($(pers2).attr("moves"));
+    player2.health = 10;
+    player2.weapon = { name: "lambda", strength: 10 };
     //player2.turn = $(pers2).attr("isP2Turn");
-    player2.turn = false
-    
+    //player2.turn = false;
+
+    isPlayer1Turn()
 
     console.log("Generation player1", player1);
     console.log("Generation player2", player2);
+    function showPlayerForce(player) {
+      console.log(player.name + " Force : " + player.weapon.strength);
+    }
 
     function checkMoves(player) {
       if (player.moves == 0) {
@@ -79,32 +106,74 @@ $(function () {
       }
     }
 
-    showInfo()
+    showInfo();
 
     function showInfo() {
       let turnInfoBox = $("#turnInfo");
       if (player1.turn == true) {
         turnInfoBox.removeClass("w3-blue").addClass("w3-pink");
-        turnInfoBox.html('<p>Tour de <span class="w3-large">' + player1.name + "</span></p><p>Nombre de mouvements restants : " + player1.moves + "</p>"); 
-      }
-      else if (player2.turn == true) {
+        turnInfoBox.html(
+          '<p>Tour de <span class="w3-large">' +
+            player1.name +
+            "</span></p><p>Nombre de mouvements restants : " +
+            player1.moves +
+            "</p><p>Force : " +
+            player1.weapon.strength +
+            "</p>"
+        );
+      } else if (player2.turn == true) {
         turnInfoBox.removeClass("w3-pink").addClass("w3-blue");
-        turnInfoBox.html('<p>Tour de <span class="w3-large">' + player2.name + "</span></p><p>Nombre de mouvements restants : " + player2.moves + "</p>"); 
+        turnInfoBox.html(
+          '<p>Tour de <span class="w3-large">' +
+            player2.name +
+            "</span></p><p>Nombre de mouvements restants : " +
+            player2.moves +
+            "</p><p>Force : " +
+            player2.weapon.strength +
+            "</p>"
+        );
       }
     }
-    
-    //turnInfoBox.html('<p>Tour de ' + player1.name + "</p><p>Nombre de mouvements restants : " + player1.moves + "</p>");
+
+    function changeTurn() {
+      console.log('debut CT player1', player1);
+        console.log('debut CT player2', player2);
+        if (player1.moves == 3 && player1.turn == true) {
+          alert("Vous devez vous déplacez au moins une fois, " + player1.name);  
+        }
+        else if (player2.moves == 3 && player2.turn == true) {
+          alert("Vous devez vous déplacez au moins une fois, " + player2.name);  
+        }
+      else if (player1.moves < 3 && player1.turn == true) {
+        console.log('P1 true');
+        player1.turn = false;
+        player2.turn = true;
+        player2.moves = 3;
+        player1.moves = 0;
+        showInfo();
+        
+      } else if (player2.moves < 3 && player2.turn == true) {
+        console.log('P2 true');
+        player2.turn = false;
+        player1.turn = true;
+        player1.moves = 3;
+        player2.moves = 0;
+        showInfo();
+      }
+      console.log('fin CT player1', player1);
+      console.log('fin CT player2', player2);
+     }
 
     move();
     function move() {
       $(document).keydown(function (e) {
         switch (e.which) {
           case 37: //left arrow key
-          console.log('Debut G player1', player1);
-          console.log('Debut G player2', player2);
+            console.log("Debut G player1", player1);
+            console.log("Debut G player2", player2);
 
-          if (player1.X > 0 && player1.moves > 0 && player1.turn == true) {
-            console.log("gauche P1 possible");
+            if (player1.X > 0 && player1.moves > 0 && player1.turn == true) {
+              console.log("gauche P1 possible");
               player1.removeClass("w3-pink").addClass("w3-lime");
               player1.X -= 1;
               let x1 = player1.X;
@@ -138,7 +207,7 @@ $(function () {
                 player1.moves++;
 
                 prevCell.removeClass("w3-lime").addClass("w3-pink");
-              } 
+              }
               //cas du déplacement sur cellule accessible
               else if (nextCell.hasClass("w3-lime")) {
                 console.log(" OK Gauche acces P1", nextCell);
@@ -146,56 +215,60 @@ $(function () {
                 console.log(" Gauche acces P1", nextCell);
               }
               console.log("finG P1", player1);
-          } else if (player2.X > 0 && player2.moves > 0 && player2.turn == true) {
-            console.log("gauche P2 possible");
-            player2.removeClass("w3-blue").addClass("w3-lime");
-            player2.X -= 1;
-            let x2 = player2.X;
-            let y2 = player2.Y;
-            let prevX2 = x2 + 1;
+            } else if (
+              player2.X > 0 &&
+              player2.moves > 0 &&
+              player2.turn == true
+            ) {
+              console.log("gauche P2 possible");
+              player2.removeClass("w3-blue").addClass("w3-lime");
+              player2.X -= 1;
+              let x2 = player2.X;
+              let y2 = player2.Y;
+              let prevX2 = x2 + 1;
 
-            player2.moves--;
-            //checkMoves(player2);
-            if (player2.moves == 0) {
-              console.log("P2 0 mov");
-              console.log("P1" + player1.turn);
-              player1.turn = true;
-              console.log("Dernier mouvement P2");
-              console.log("P1" + player1.turn);
-              console.log("P2" + player2.turn);
-              player2.turn = false;
-              player1.moves = 3;
-              console.log("P2" + player2.turn);
-              //commencer tour suivant
+              player2.moves--;
+              //checkMoves(player2);
+              if (player2.moves == 0) {
+                console.log("P2 0 mov");
+                console.log("P1" + player1.turn);
+                player1.turn = true;
+                console.log("Dernier mouvement P2");
+                console.log("P1" + player1.turn);
+                console.log("P2" + player2.turn);
+                player2.turn = false;
+                player1.moves = 3;
+                console.log("P2" + player2.turn);
+                //commencer tour suivant
+              }
+              let prevCell = $(
+                "td[x= " + prevX2.toString() + "][y= " + y2.toString() + "]"
+              );
+              let nextCell = $(
+                "td[x= " + x2.toString() + "][y= " + y2.toString() + "]"
+              );
+              console.log(" normal deplacement Gauche prevXPos P2", prevX2);
+              console.log(" nextCell P2", nextCell);
+              prevCell.removeClass("w3-blue").addClass("w3-lime");
+
+              //cas du déplacement sur cellule inaccessible
+              if (nextCell.hasClass("w3-dark-gray")) {
+                console.log("non access GP2 ");
+                nextCell.effect("shake", { times: 4 }, 1000);
+                console.log("player2", player2);
+                player2.X++;
+                player2.moves++;
+
+                prevCell.removeClass("w3-lime").addClass("w3-blue");
+              }
+              //cas du déplacement sur cellule accessible
+              else if (nextCell.hasClass("w3-lime")) {
+                console.log(" OK Gauche acces P2", nextCell);
+                nextCell.removeClass("w3-lime").addClass("w3-blue");
+                console.log(" Gauche acces P2", nextCell);
+              }
+              console.log("fin G P2", player2);
             }
-            let prevCell = $(
-              "td[x= " + prevX2.toString() + "][y= " + y2.toString() + "]"
-            );
-            let nextCell = $(
-              "td[x= " + x2.toString() + "][y= " + y2.toString() + "]"
-            );
-            console.log(" normal deplacement Gauche prevXPos P2", prevX2);
-            console.log(" nextCell P2", nextCell);
-            prevCell.removeClass("w3-blue").addClass("w3-lime");
-
-            //cas du déplacement sur cellule inaccessible
-            if (nextCell.hasClass("w3-dark-gray")) {
-              console.log("non access GP2 ");
-              nextCell.effect("shake", { times: 4 }, 1000);
-              console.log("player2", player2);
-              player2.X++;
-              player2.moves++;
-
-              prevCell.removeClass("w3-lime").addClass("w3-blue");
-            } 
-            //cas du déplacement sur cellule accessible
-            else if (nextCell.hasClass("w3-lime")) {
-              console.log(" OK Gauche acces P2", nextCell);
-              nextCell.removeClass("w3-lime").addClass("w3-blue");
-              console.log(" Gauche acces P2", nextCell);
-            }
-            console.log("fin G P2", player2);
-          }
             break;
 
           case 38: //up arrow key
@@ -216,7 +289,7 @@ $(function () {
                 console.log("P2" + player2.turn);
                 //commencer tour suivant
               }
-              
+
               let prevCell = $(
                 "td[x= " + x1.toString() + "][y= " + prevY1.toString() + "]"
               );
@@ -239,8 +312,11 @@ $(function () {
                 console.log("P1 OK acces Haut");
                 nextCell.removeClass("w3-lime").addClass("w3-pink");
               }
-            }
-            else if (player2.Y > 0 && player2.moves > 0 && player2.turn == true) {
+            } else if (
+              player2.Y > 0 &&
+              player2.moves > 0 &&
+              player2.turn == true
+            ) {
               console.log("H P2 possible");
               player2.Y -= 1;
               let x2 = player2.X;
@@ -257,16 +333,16 @@ $(function () {
                 //commencer tour suivant
               }
 
-                let prevCell = $(
-                  "td[x= " + x2.toString() + "][y= " + prevY2.toString() + "]"
-                );
-                let nextCell = $(
-                  "td[x= " + x2.toString() + "][y= " + y2.toString() + "]"
-                );
-                console.log("P2 normal deplacement Haut prevY2", prevY2);
-                prevCell.removeClass("w3-blue").addClass("w3-lime");
-                
-                //cas du déplacement sur cellule inaccessible
+              let prevCell = $(
+                "td[x= " + x2.toString() + "][y= " + prevY2.toString() + "]"
+              );
+              let nextCell = $(
+                "td[x= " + x2.toString() + "][y= " + y2.toString() + "]"
+              );
+              console.log("P2 normal deplacement Haut prevY2", prevY2);
+              prevCell.removeClass("w3-blue").addClass("w3-lime");
+
+              //cas du déplacement sur cellule inaccessible
               if (nextCell.hasClass("w3-dark-gray")) {
                 console.log("P2 non access Haute");
                 nextCell.effect("shake", { times: 4 }, 1000);
@@ -280,18 +356,20 @@ $(function () {
                 console.log("P2 OK acces Haut");
                 nextCell.removeClass("w3-lime").addClass("w3-blue");
               }
-              
             }
             console.log("fin HP1 ", player1);
             console.log("fin HP2 ", player2);
-            
             break;
 
           case 39: //right arrow key
             //console.log("debut D", player1.X);
             console.log("Tour P1 D", player1.turn);
             console.log("Tour P1 D", player1.turn);
-            if (player1.X < nbCols - 1 && player1.moves > 0 && player1.turn == true) {
+            if (
+              player1.X < nbCols - 1 &&
+              player1.moves > 0 &&
+              player1.turn == true
+            ) {
               console.log("P1 droite possible");
               player1.removeClass("w3-pink").addClass("w3-lime");
               player1.X += 1;
@@ -325,15 +403,17 @@ $(function () {
                 player1.moves++;
 
                 prevCell.removeClass("w3-lime").addClass("w3-pink");
-              } 
+              }
               //cas du déplacement sur cellule accessible
               else if (nextCell.hasClass("w3-lime")) {
                 console.log("OK Droite access P1");
                 nextCell.removeClass("w3-lime").addClass("w3-pink");
               }
-            }
-            
-            else if(player2.X < nbCols - 1 && player2.moves > 0 && player2.turn == true) {
+            } else if (
+              player2.X < nbCols - 1 &&
+              player2.moves > 0 &&
+              player2.turn == true
+            ) {
               console.log("P2 droite possible");
               player2.removeClass("w3-blue").addClass("w3-lime");
               player2.X += 1;
@@ -366,7 +446,7 @@ $(function () {
                 player2.moves++;
 
                 prevCell.removeClass("w3-lime").addClass("w3-blue");
-              } 
+              }
               //cas du déplacement sur cellule accessible
               else if (nextCell.hasClass("w3-lime")) {
                 console.log("OK Droite access P2");
@@ -381,7 +461,11 @@ $(function () {
             console.log("debut P1 B", player1.turn);
             console.log("debut P2 B", player2.turn);
 
-            if (player1.Y < nbRows - 1 && player1.moves > 0 && player1.turn == true) {
+            if (
+              player1.Y < nbRows - 1 &&
+              player1.moves > 0 &&
+              player1.turn == true
+            ) {
               console.log("Bas P1 possible");
               player1.Y += 1;
               let x1 = player1.X;
@@ -415,13 +499,16 @@ $(function () {
                 player1.moves++;
 
                 prevCell.removeClass("w3-lime").addClass("w3-pink");
-              }//cas du déplacement sur cellule accessible 
+              } //cas du déplacement sur cellule accessible
               else if (nextCell.hasClass("w3-lime")) {
                 console.log("P1 OK Bas access");
                 nextCell.removeClass("w3-lime").addClass("w3-pink");
               }
-            }
-            else if (player2.Y < nbRows - 1 && player2.moves > 0 && player2.turn == true) {
+            } else if (
+              player2.Y < nbRows - 1 &&
+              player2.moves > 0 &&
+              player2.turn == true
+            ) {
               console.log("Bas P2 possible");
               player2.Y += 1;
               let x2 = player2.X;
@@ -453,13 +540,10 @@ $(function () {
                 nextCell.effect("shake", { times: 4 }, 1000);
                 player2.Y--;
                 player2.moves++;
-                //TODO check turn state for players
-                /* player1.turn = false;
-                player2.turn = true; */
-
+               
                 prevCell.removeClass("w3-lime").addClass("w3-blue");
               }
-              //cas du déplacement sur cellule accessible 
+              //cas du déplacement sur cellule accessible
               else if (nextCell.hasClass("w3-lime")) {
                 console.log("P2 OK Bas access");
                 nextCell.removeClass("w3-lime").addClass("w3-blue");
@@ -472,15 +556,21 @@ $(function () {
         }
         console.log(player1);
         console.log(player2);
-        //TODO attribuer player attr à pers1
         console.log("player1Turn", player1.turn);
         console.log("player2Turn", player2.turn);
         showInfo();
         //console.log("player1Turn", player1.turn);
       });
+      $("#nextBtn").click(function() {
+        // Changer de tour
+        changeTurn();
+      })
     }
 
+   
   }
+
+ 
 
   $("#startBtn").click(function () {
     //Réinitialiser la grille

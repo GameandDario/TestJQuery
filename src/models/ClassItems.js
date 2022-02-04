c32ca5b783c17bf557f9e068f3692fe789eb8c32
Stéphane <stephane.sokeo@gmail.com>
2022-02-04 18:35:34 +0100

    add ClassItems

--- src/models/ClassItems.js
+++ src/models/ClassItems.js
@@ -24,3 +24,18 @@ class Item {
     }
 
 }
+function prepareItem(element, object, instance) {
+    element
+      .removeClass("w3-lime")
+      .addClass("w3-brown")
+      .removeAttr("accessible");
+    element.X = parseInt(object.attr("x"));
+    element.Y = parseInt(object.attr("y"));
+    element.name = instance.name;
+    element.strength = instance.strength;
+    element.available = true;
+  }
+  const item1 = new Item("Hache", 15, "hache.jpg");
+  const item2 = new Item("Kunaï", 20, "kunai.jpg");
+  const item3 = new Item("Grenade", 25, "grenade.jpg");
+  const item4 = new Item("Cobra", 30, "cobra.jpg");
